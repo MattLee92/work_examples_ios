84db86fef1a3d9178f344b9e1de935daa913ceea
Matthew Lee <BairSTILLE@Matthews-MacBook-Air.local>
2015-04-19 15:27:01 +1000

    Initial Commit

---
+++ PhotoCollection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoCollection
+//
+//  Created by Matthew Lee on 19/04/2015.
+//  Copyright (c) 2015 Matthew Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
